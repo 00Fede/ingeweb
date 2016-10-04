916a859f034a41fd5c54bef5b181a0491fb34ba0
00Fede <feedkiko@gmail.com>
2016-10-04 13:36:49 -0500

    ejemplo Spring

--- SpringHibernate/src/co/edu/udea/iw/dao/hibernate/test/CiudadDaoHibernateTest.java
+++ SpringHibernate/src/co/edu/udea/iw/dao/hibernate/test/CiudadDaoHibernateTest.java
@@ -2,19 +2,22 @@ package co.edu.udea.iw.dao.hibernate.test;
 
 import static org.junit.Assert.*;
 
+
 import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import co.edu.udea.iw.dao.CiudadDao;
 import co.edu.udea.iw.dao.hibernate.CiudadDaoHibernate;
 import co.edu.udea.iw.dto.Ciudad;
 import co.edu.udea.iw.exception.MyDaoException;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"spring_config.xml"})
+@ContextConfiguration(locations={"classpath:spring_config.xml"})
 
 public class CiudadDaoHibernateTest {
 	
@@ -23,17 +26,18 @@ public class CiudadDaoHibernateTest {
 
 	@Test
 	public void testObtener() {
-		CiudadDao ciudadDao = new CiudadDaoHibernate();
+		//CiudadDao ciudadDao = new CiudadDaoHibernate();
 		
 		List<Ciudad> ciudades;
 		
 		try{
-			ciudades = ciudadDao.obtener();
+			ciudades = dao.obtener();
 			assertTrue(ciudades.size() > 0);
 		}catch(MyDaoException e){
 			e.printStackTrace();
 			fail(e.getMessage());
 		}
 	}
+
 
 }
